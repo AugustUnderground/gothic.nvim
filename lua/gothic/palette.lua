74e56271bb55da57c32ece4caaa88d3e5356a8b5
Yannick Uhlmann <yannick.uhlmann@reutlingen-university.de>
2021-05-05 19:26:16 +0200

    formatting

--- lua/gothic/palette.lua
+++ lua/gothic/palette.lua
@@ -3,60 +3,64 @@
 -- =    Color palette   =
 -- =====================
 
-local palette = {
+local palette = { color10   = '#000000'
+                , color25   = '#10100e'
+                , color11   = '#121212'
+                , color48   = '#141412'
+                , color1    = '#282828'
+                , color9    = '#262626'
+                , color14   = '#3a3a3a'
+                , color15   = '#4e4e4e'
+                , color12   = '#626262'
+                , color47   = '#666666'
+                , color23   = '#696969'
+                , color42   = '#808080'
+                , color21   = '#999999'
+                , color46   = '#a9a9a9'
+                , color32   = '#c6c6c4'
+                , color45   = '#d3d3d3'
+                , color41   = '#fffafa'
+                , color36   = '#ffffff'
 
-    color0 = '#b16286',
-    color1 = '#282828',
-    color2 = '#458588',
-    color3 = '#98971a',
-    color4 = '#928374',
-    color5 = '#a89984',
-    color6 = '#689d6a',
-    color7 = '#d79921',
-    color8 = '#cc241d',
-    color9 = '#262626',
-   color10 = '#000000',
-   color11 = '#121212',
-   color12 = '#626262',
-   color13 = '#5f87af',
-   color14 = '#3a3a3a',
-   color15 = '#4e4e4e',
-   color16 = '#ff0000',
-   color17 = '#ff922b',
-   color18 = '#fab005',
-   color19 = '#15aabf',
-   color20 = '#fb4934',
-   color21 = '#999999',
-   color22 = '#009f6b',
-   color23 = '#696969',
-   color24 = '#80a0ff',
-   color25 = '#10100e',
-   color26 = '#ee7f2d',
-   color27 = '#0087bd',
-   color28 = '#afff5f',
-   color29 = '#5fffff',
-   color30 = '#c40233',
-   color31 = '#20b2aa',
-   color32 = '#c6c6c4',
-   color33 = '#ffd700',
-   color34 = '#00ffff',
-   color35 = '#0000ff',
-   color36 = '#ffffff',
-   color37 = '#800020',
-   color38 = '#2e8b57',
-   color39 = '#008000',
-   color40 = '#ff00ff',
-   color41 = '#fffafa',
-   color42 = '#808080',
-   color43 = '#fdff00',
-   color44 = '#007fff',
-   color45 = '#d3d3d3',
-   color46 = '#a9a9a9',
-   color47 = '#666666',
-   color48 = '#141412',
-   color49 = '#ffff00',
-   color50 = '#9a4eae',
-   color51 = '#ff1493',
-}
+                , color37   = '#800020'
+                , color30   = '#c40233'
+                , color8    = '#cc241d'
+                , color16   = '#ff0000'
+                , color20   = '#fb4934'
+
+                , color38   = '#2e8b57'
+                , color22   = '#009f6b'
+                , color39   = '#008000'
+                , color6    = '#689d6a'
+                , color28   = '#afff5f'
+                , color3    = '#98971a'
+
+                , color4    = '#928374'
+                , color5    = '#a89984'
+                , color7    = '#d79921'
+                , color26   = '#ee7f2d'
+                , color17   = '#ff922b'
+                , color18   = '#fab005'
+                , color33   = '#ffd700'
+                , color43   = '#fdff00'
+                , color49   = '#ffff00'
+
+                , color2    = '#458588'
+                , color13   = '#5f87af'
+                , color24   = '#80a0ff'
+                , color27   = '#0087bd'
+                , color44   = '#007fff'
+                , color35   = '#0000ff'
+                
+                , color0    = '#b16286'
+                , color50   = '#9a4eae'
+                , color51   = '#ff1493'
+                , color40   = '#ff00ff'
+
+                , color31   = '#20b2aa'
+                , color19   = '#15aabf'
+                , color29   = '#5fffff'
+                , color34   = '#00ffff'
+                , }
 
 return palette
